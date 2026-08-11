bb6a171663e11d5eb29941d1af864f5cce1112c8
Gustavo Jácome <155338841+gustavofjacome@users.noreply.github.com>
2026-08-11 20:47:06 -0300

    iniciando implementação de pilha baseada em array

---
+++ pilhaArray/src/Pilha.java
@@ -0,0 +1,11 @@
+public class Pilha {
+    private int topo;
+    private String[] pilha;
+
+    public Pilha(int capacidade) {
+        this.topo = -1;
+        this.pilha = new String[capacidade];
+    }
+    //TODO: push(), pop, peek, top , is empty, is full, size;
+
+}
