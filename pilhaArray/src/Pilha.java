d36da4338c4e84cdf7a8ae48865fc6f456f4ca75
Gustavo Jácome <155338841+gustavofjacome@users.noreply.github.com>
2026-08-11 20:54:24 -0300

    is empty e push, ainda falta as exceções

--- pilhaArray/src/Pilha.java
+++ pilhaArray/src/Pilha.java
@@ -8,4 +8,13 @@ public class Pilha {
     }
     //TODO: push(), pop, peek, top , is empty, is full, size;
 
+    public boolean isEmpty(){
+        return this.topo == -1;
+    }
+
+
+    public void push(String elemento){
+        this.pilha[++topo] = elemento;
+    }
+
 }
