01c1816afb0e0782c4652e9b1ffa30c3d756a84c
Gustavo Jácome <155338841+gustavofjacome@users.noreply.github.com>
2026-08-11 21:20:33 -0300

    testando

---
+++ pilhaArray/src/Testes.java
@@ -0,0 +1,31 @@
+public class Testes {
+    public static void main(String[] args) {
+        Pilha prato = new Pilha(5);
+
+        System.out.println(prato.isEmpty());
+        System.out.println(prato.isFull());
+
+        //=================
+
+        prato.push("B");
+        prato.push("R");
+        prato.push("A");
+        prato.push("S");
+        prato.push("A");
+
+        System.out.println(prato.isEmpty());
+        System.out.println(prato.isFull());
+
+
+        prato.pop();
+        prato.pop();
+        prato.pop();
+        prato.pop();
+        prato.pop();
+
+        System.out.println(prato.isEmpty());
+        System.out.println(prato.isFull());
+
+
+    }
+}
