68a6688349869580c7cf9848d4dc113916cf1cb0
Gustavo Jácome <155338841+gustavofjacome@users.noreply.github.com>
2026-08-11 22:58:19 -0300

    refatorando a pilha para utilizar interface e tipo Object
    Apenas para ficar mais fiel a implementação do professor

--- pilhaArray/src/Pilha.java
+++ pilhaArray/src/Pilha.java
@@ -1,54 +1,8 @@
-public class Pilha<T> {
-    private int topo;
-    private T[] pilha;
-    private int size;
-
-    public Pilha(int capacidade) {
-        this.topo = -1;
-        this.pilha = (T[]) new Object[capacidade];
-        this.size = 0;
-    }
-
-    public boolean isEmpty(){
-        return this.topo == -1; // -1 é o valor padrao do construtor la em cima se ele ta -1 é pq nao foi alterado, logo ta vazio
-    }
-
-    public boolean isFull(){
-        return this.topo+1 == pilha.length;
-    }
-
-    //adiciona elemento em cima
-    public void push(T elemento) throws Exception {
-        if (isFull()){
-            throw new Exception(new PilhaCheiaException("Pilha Cheia"));
-        }
-        this.pilha[++topo] = elemento;
-        size++;
-    }
-
-    //retorna o de cima e remove
-    public T pop() throws Exception {
-        if (isEmpty()){
-            throw new Exception(new PilhaVaziaException("Pilha Vazia"));
-        }
-        T isRemove = this.pilha[topo];
-        this.pilha[topo] = null;
-        topo--;
-        size--;
-        return isRemove;
-    }
-
-
-    //retorna o de cima sem remover
-    public T top() throws Exception {
-        if (isEmpty()){
-            throw new Exception(new PilhaVaziaException("Pilha Vazia"));
-        }
-        return this.pilha[topo];
-    }
-
-    public int size(){
-        return size;
-    }
-
+public interface Pilha {
+    public int size();
+    public boolean isEmpty();
+    public boolean isFull();
+    public Object top() throws PilhaVaziaException;
+    public void push(Object o);
+    public Object pop() throws PilhaVaziaException;
 }
---
+++ pilhaArray/src/PilhaArray.java
@@ -0,0 +1,79 @@
+import Exception.*;
+
+public class PilhaArray<T> implements Pilha {
+    private int capacity;
+    private Object[] a;
+    private int head;
+    private int fc; // fator de crescimento quando encher vai ser usado para aumentar a capacidade
+
+    public PilhaArray(int capacidade, int fatorCrescimento) {
+        this.capacity = capacidade;
+        head = -1;
+        this.fc = fatorCrescimento;
+        if (fatorCrescimento <= 0){
+            fc = 0;
+        }
+        a = new Object[capacidade];
+    }
+    @Override
+    public boolean isEmpty(){
+        return this.head == -1; // -1 é o valor padrao do construtor la em cima se ele ta -1 é pq nao foi alterado, logo ta vazio
+    }
+
+    @Override
+    public boolean isFull(){
+        return this.head+1 == a.length;
+    }
+
+    @Override
+    public int size(){
+        return this.head+1;
+    }
+
+    //adiciona elemento em cima
+    @Override
+    public void push(Object objeto) {
+        if (increaseCapacity()){
+            Object[] b = new Object[capacity];
+            for (int i = 0; i < a.length; i++) {
+                b[i] = a[i];
+            }
+            this.a = b;
+        }
+        this.a[++head] = objeto;
+    }
+
+    //retorna o de cima e remove
+    @Override
+    public Object pop() {
+        if (isEmpty()) {
+            throw new PilhaVaziaException("Pilha Vazia");
+        }
+        Object isRemove = a[head];
+        a[head] = null;
+        head--;
+        return isRemove;
+    }
+
+    //retorna o de cima sem remover
+    public Object top() {
+        if (isEmpty()) {
+            throw new PilhaVaziaException("Pilha Vazia");
+        }
+        return this.a[head];
+    }
+
+    public boolean increaseCapacity() {
+        if (isFull()){
+            if (fc == 0) {
+                capacity *= 2;
+            } else {
+                capacity += fc;
+            }
+            return true;
+        }
+        return false;
+    }
+
+
+}
