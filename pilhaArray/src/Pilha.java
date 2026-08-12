a36230ee62d574961f0368207d4ae860b799edf1
Gustavo Jácome <155338841+gustavofjacome@users.noreply.github.com>
2026-08-11 21:14:22 -0300

    adicionando size

--- pilhaArray/src/Pilha.java
+++ pilhaArray/src/Pilha.java
@@ -1,32 +1,45 @@
 public class Pilha {
     private int topo;
     private String[] pilha;
+    private int size;
 
     public Pilha(int capacidade) {
         this.topo = -1;
         this.pilha = new String[capacidade];
+        this.size = 0;
     }
 
     public boolean isEmpty(){
-        return this.topo == -1;
+        return this.topo == -1; // -1 é o valor padrao do construtor la em cima se ele ta -1 é pq nao foi alterado, logo ta vazio
     }
 
     public boolean isFull(){
         return this.topo+1 == pilha.length;
     }
 
+    //adiciona elemento em cima
     public void push(String elemento){
         this.pilha[++topo] = elemento;
+        size++;
     }
 
+    //retorna o de cima e remove
     public String pop(){
         String isRemove = this.pilha[topo];
         this.pilha[topo] = " ";
+        size--;
         return isRemove;
     }
 
+
+    //retorna o de cima sem remover
     public String top(){
         return this.pilha[topo];
+    }
+
+
+    public int size(){
+        return size;
     }
 
 }
