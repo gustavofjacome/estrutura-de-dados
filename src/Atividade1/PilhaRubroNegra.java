9582a4251ea4ad08ba7a0769b5ea4b04689d7aaa
Gustavo Jácome <155338841+gustavofjacome@users.noreply.github.com>
2026-08-18 21:35:19 -0300

    amanhã termino isso 🥱🛏️💤😴🥱🥱🥱

---
+++ src/Atividade1/PilhaRubroNegra.java
@@ -0,0 +1,49 @@
+package src.Atividade1;
+
+public class PilhaRubroNegra {
+    private Object array;
+    private int rubroHead;
+    private int negraHead;
+
+    public PilhaRubroNegra(int capacidade) {
+        array = new Object[capacidade];
+
+        rubroHead = -1; // começo
+        negraHead = capacidade; // final
+    }
+
+    // TODO:pushVermelha()
+
+    public void pushRubro(Object objeto){
+
+    }
+
+    // TODO:pushPreta()
+
+
+
+
+
+
+
+
+
+
+
+
+    // TODO:popVermelha()
+    // TODO:popPreta()
+
+
+
+
+
+
+
+
+
+    // TODO:topVermelha()
+    // TODO:topPreta()
+
+    // TODO:redimensionar()
+}
